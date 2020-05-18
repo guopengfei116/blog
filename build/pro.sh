b5876fa70d6de4417eb8953d69efe020b86243b5
guopf <626138959@qq.com>
2020-05-19 00:42:02 +0800

    feat: build script and theme

---
+++ build/pro.sh
@@ -0,0 +1,10 @@
+echo "打包中..."
+npm run docs:build
+
+echo "打包完成，正在部署..."
+cp -r docs/.vuepress/dist /tmp/blog
+
+echo "正在重启静态服务..."
+nginx -s reload
+
+echo "博客系统部署完毕，请访问www.ggpf.top/blog查阅！"
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: '我们',
+  description: '程序猿的技术历程',
+  base: "/blog/",
+  themeConfig: {
+    nav: [
+      { text: 'vue', link: '/vue/guide' },
+      { text: 'react', link: '/react/guide' },
+      { text: 'webpack', link: '/webpack/guide' },
+    ]
+  }
+}
