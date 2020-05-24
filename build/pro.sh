f8c171887db16f03e758b8dc047108cf28fa37d0
guopf <626138959@qq.com>
2020-05-25 02:22:30 +0800

    chore: 部署脚本修改

--- build/pro.sh
+++ build/pro.sh
@@ -2,9 +2,10 @@ echo "打包中..."
 npm run docs:build
 
 echo "打包完成，正在部署..."
+rm -rf /tmp/blog
 cp -rf docs/.vuepress/dist /tmp/blog
 
-echo "正在重启静态服务..."
+echo "重启静态服务..."
 nginx -s reload
 
 echo "博客系统部署完毕，请访问www.ggpf.top/blog查阅！"
