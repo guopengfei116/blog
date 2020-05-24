080eac68b86585897486371906c569ade409c6a3
guopf <626138959@qq.com>
2020-05-25 02:13:22 +0800

    feat: 首页修改,导航分类增加

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,70 @@
 module.exports = {
-  title: '我们',
+  title: 'Home',
   description: '程序猿的技术历程',
   base: "/blog/",
   themeConfig: {
+    sidebar: 'auto',
     nav: [
-      { text: 'vue', link: '/vue/guide' },
-      { text: 'react', link: '/react/guide' },
-      { text: 'webpack', link: '/webpack/guide' },
+      {
+        text: "前端Web",
+        items: [
+          { text: 'vue', link: '/frontend-web/vue/guide' },
+          { text: 'react', link: '/frontend-web/react/guide' },
+          { text: 'webpack', link: '/frontend-web/webpack/guide' },
+        ]
+      },
+      // {
+      //   text: "后端",
+      //   items: [
+      //     {
+      //       text: "Java",
+      //       items: [
+      //         { text: 'vue', link: '/frontend-web/vue/guide' },
+      //       ]
+      //     },
+      //     {
+      //       text: "Python",
+      //       items: [
+      //         { text: 'vue', link: '/frontend-web/vue/guide' },
+      //       ]
+      //     },
+      //   ]
+      // },
+      // {
+      //   text: "数据库",
+      //   items: [
+      //     { text: 'vue', link: '/frontend-web/vue/guide' },
+      //   ]
+      // },
+      // {
+      //   text: "大数据",
+      //   items: [
+      //     { text: 'vue', link: '/frontend-web/vue/guide' },
+      //   ]
+      // },
+      {
+        text: "算法与数据结构",
+        items: [
+          {
+            text: "算法",
+            items: [
+              { text: '效率', link: '/algorithm-design/algorithm/big-o' },
+            ]
+          },
+          {
+            text: "数据结构",
+            items: [
+              { text: '线性表', link: '/algorithm-design/structure/linear-list' },
+            ]
+          }
+        ]
+      },
+      {
+        text: "设计模式",
+        items: [
+          { text: 'vue', link: '/frontend-web/vue/guide' },
+        ]
+      },
     ]
   }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,16 +1,23 @@
 ---
 home: true
-heroImage: /hero.png
-heroText: Hero 标题
-tagline: Hero 副标题
-actionText: 快速上手 →
-actionLink: /zh/guide/
+heroText: 学习笔记
+tagline: 面向生活编程
+actionText: 开始学习 →
+actionLink: /frontend-web
 features:
-- title: 简洁至上
-  details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
-- title: Vue驱动
-  details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
-- title: 高性能
-  details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
-footer: MIT Licensed | Copyright © 2018-present Evan You
+- title: 深度
+  details: 不局限与技术应用，从表象到本质，从原理出发看透技术。
+- title: 广度
+  details: 不局限与特定技术栈，只享受学习技术与分享学习心得的过程。
+- title: 深入浅出
+  details: 再复杂的技术也能够转换成若干简单的问题，能够用浅显易懂的方式描述技术，并让别人理解，是检验自己技术理解程度的重要方式。
 ---
+
+<div class="footer">
+  <p>MIT Licensed | Copyright © 2020-2030 www.ggpf.top All Rights Reserved</p>
+  <p>
+    <a href="http://www.beian.miit.gov.cn/" target="_blank">
+      <span style="color:#000;">粤ICP备17019741号</span>
+    </a>
+  </p>
+</div>
